9999e0c5e8506f191746c90889a1f0091218c49f
coolsam726 <coolsam726@users.noreply.github.com>
2024-04-06 16:44:55 +0000

    Fix styling

--- src/Commands/SeederMakeCommand.php
+++ src/Commands/SeederMakeCommand.php
@@ -10,6 +10,7 @@ class SeederMakeCommand extends \Illuminate\Database\Console\Seeds\SeederMakeCom
     use GeneratesModularFiles;
 
     protected $name = 'modular:make-seeder';
+
     protected $description = 'Create a new seeder class in a modular package';
 
     protected function rootNamespace(): string
@@ -25,6 +26,7 @@ class SeederMakeCommand extends \Illuminate\Database\Console\Seeds\SeederMakeCom
     protected function getPath($name): string
     {
         $name = str_replace('\\', '/', Str::replaceFirst($this->rootNamespace(), '', $name));
+
         return $this->getModule()->seedersPath($name.'.php');
     }
 }
