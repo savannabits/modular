504f24b46f8ae112a8767d3993e7df4fd1247fec
coolsam726 <coolsam726@users.noreply.github.com>
2024-04-07 19:43:48 +0000

    Fix styling

--- src/Commands/RequestMakeCommand.php
+++ src/Commands/RequestMakeCommand.php
@@ -7,12 +7,13 @@ use Savannabits\Modular\Support\Concerns\GeneratesModularFiles;
 class RequestMakeCommand extends \Illuminate\Foundation\Console\RequestMakeCommand
 {
     use GeneratesModularFiles;
+
     protected $name = 'modular:make-request';
+
     protected $description = 'Create a new form request class in a modular package';
 
     protected function getRelativeNamespace(): string
     {
         return '\\Http\\Requests';
     }
-
 }
