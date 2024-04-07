1a3bc0395a49dcbfb6db5f3274c4881b46022dda
coolsam726 <coolsam726@users.noreply.github.com>
2024-04-07 19:27:27 +0000

    Fix styling

--- src/Commands/PolicyMakeCommand.php
+++ src/Commands/PolicyMakeCommand.php
@@ -7,7 +7,9 @@ use Savannabits\Modular\Support\Concerns\GeneratesModularFiles;
 class PolicyMakeCommand extends \Illuminate\Foundation\Console\PolicyMakeCommand
 {
     use GeneratesModularFiles;
+
     protected $name = 'modular:make-policy';
+
     protected $description = 'Create a new policy class in a modular package';
 
     protected function getRelativeNamespace(): string
